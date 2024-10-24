fbb7b59d38daabc1d214c7f56f1e8ce8689fd2a3
arrayappy <arrayappy@gmail.com>
2024-10-24 19:33:02 +0530

    feat(vault): added program

---
+++ README.md
@@ -0,0 +1,5 @@
+## Solana Native Rust Q4 Programs
+
+### Vault
+
+A simple vault program that allows a user to deposit and withdraw SOL.
---
+++ vault/src/deposit.rs
@@ -0,0 +1,13 @@
+use solana_program::{
+  account_info::AccountInfo, entrypoint::ProgramResult, program::invoke,
+  program_error::ProgramError, system_instruction::transfer,
+};
+
+/// Deposit funds into vault with deterministic address derived from Signer's pubkey
+pub fn process(accounts: &[AccountInfo<'_>], amount: u64) -> ProgramResult {
+  let [signer, vault, _system_program] = accounts else {
+      return Err(ProgramError::NotEnoughAccountKeys);
+  };
+
+  invoke(&transfer(signer.key, vault.key, amount), accounts)
+}
