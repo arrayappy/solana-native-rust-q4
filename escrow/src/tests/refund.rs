772d0817e81c0ed39763d6b5e81076e1f30acb6f
arrayappy <arrayappy@gmail.com>
2024-11-12 21:20:53 +0530

    test(escrow): all tests working

---
+++ escrow/src/tests/refund.rs
@@ -0,0 +1,150 @@
+use std::mem;
+use mollusk_svm::Mollusk;
+use solana_sdk::{
+    account::{AccountSharedData, WritableAccount},
+    instruction::{AccountMeta, Instruction},
+    program_option::COption,
+    program_pack::Pack,
+    pubkey::Pubkey,
+};
+use spl_token::state::AccountState;
+use crate::state::Escrow;
+
+#[test]
+fn refund() {
+    let program_id = Pubkey::new_from_array(five8_const::decode_32_const(
+        "ESCr1yhmVUrX8vRURmZRnGM4QugeFMHvY2ABUa2Cewrh",
+    ));
+
+    let mut mollusk = Mollusk::new(&program_id, "target/deploy/escrow");
+
+    mollusk_token::token::add_program(&mut mollusk);
+    let (token_program, token_program_account) = mollusk_token::token::keyed_account();
+
+    let maker = Pubkey::new_unique();
+    let maker_ta_a = Pubkey::new_unique();
+    let escrow = Pubkey::new_unique();
+    let vault = Pubkey::new_unique();
+    let (authority, bump) =
+        Pubkey::try_find_program_address(&[escrow.as_ref()], &program_id).unwrap();
+    let mint_a = Pubkey::new_unique();
+    let maker_ta_b = Pubkey::new_unique();
+    let mint_b = Pubkey::new_unique();
+
+    let mut mint_a_account = AccountSharedData::new(
+        mollusk
+            .sysvars
+            .rent
+            .minimum_balance(spl_token::state::Mint::LEN),
+        spl_token::state::Mint::LEN,
+        &token_program,
+    );
+    solana_sdk::program_pack::Pack::pack(
+        spl_token::state::Mint {
+            mint_authority: COption::None,
+            supply: 100_000_000_000,
+            decimals: 6,
+            is_initialized: true,
+            freeze_authority: COption::None,
+        },
+        mint_a_account.data_as_mut_slice(),
+    )
+    .unwrap();
+
+    let mut maker_ta_a_account = AccountSharedData::new(
+        mollusk
+            .sysvars
+            .rent
+            .minimum_balance(spl_token::state::Account::LEN),
+        spl_token::state::Account::LEN,
+        &token_program,
+    );
+    solana_sdk::program_pack::Pack::pack(
+        spl_token::state::Account {
+            mint: mint_a,
+            owner: maker,
+            amount: 0,
+            delegate: COption::None,
+            state: AccountState::Initialized,
+            is_native: COption::None,
+            delegated_amount: 0,
+            close_authority: COption::None,
+        },
+        maker_ta_a_account.data_as_mut_slice(),
+    )
+    .unwrap();
+
+    let mut vault_account = AccountSharedData::new(
+        mollusk
+            .sysvars
+            .rent
+            .minimum_balance(spl_token::state::Account::LEN),
+        spl_token::state::Account::LEN,
+        &token_program,
+    );
+    solana_sdk::program_pack::Pack::pack(
+        spl_token::state::Account {
+            mint: mint_a,
+            owner: authority,
+            amount: 1_000_000,
+            delegate: COption::None,
+            state: AccountState::Initialized,
+            is_native: COption::None,
+            delegated_amount: 0,
+            close_authority: COption::None,
+        },
+        vault_account.data_as_mut_slice(),
+    )
+    .unwrap();
+
+    let mut escrow_account = AccountSharedData::new(
+        mollusk
+            .sysvars
+            .rent
+            .minimum_balance(mem::size_of::<Escrow>()),
+        mem::size_of::<Escrow>(),
+        &program_id,
+    );
+    escrow_account.set_data_from_slice(
+        &[
+            maker.to_bytes().to_vec(),
+            maker_ta_b.to_bytes().to_vec(),
+            mint_a.to_bytes().to_vec(),
+            mint_b.to_bytes().to_vec(),
+            1_000_000u64.to_le_bytes().to_vec(),
+        ]
+        .concat(),
+    );
+
+    let data = [2, bump];
+
+    let instruction = Instruction::new_with_bytes(
+        program_id,
+        &data,
+        vec![
+            AccountMeta::new(maker, true),
+            AccountMeta::new(maker_ta_a, false),
+            AccountMeta::new(escrow, false),
+            AccountMeta::new(vault, false),
+            AccountMeta::new(authority, true),
+            AccountMeta::new(token_program, false),
+        ],
+    );
+
+    let result: mollusk_svm::result::InstructionResult = mollusk.process_instruction(
+        &instruction,
+        &vec![
+            (
+                maker,
+                AccountSharedData::new(1_000_000_000, 0, &Pubkey::default()),
+            ),
+            (maker_ta_a, maker_ta_a_account),
+            (escrow, escrow_account),
+            (vault, vault_account),
+            (authority, AccountSharedData::new(0, 0, &Pubkey::default())),
+            (token_program, token_program_account),
+        ],
+    );
+
+    assert!(!result.program_result.is_err());
+} 
