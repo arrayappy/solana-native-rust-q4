36212bf7185540ed38c16f4891edd69dba582b13
arrayappy <arrayappy@gmail.com>
2024-11-18 23:49:51 +0530

    feat(spbf-close): add program and test

--- sbpf-close/src/lib.rs
+++ sbpf-close/src/lib.rs
@@ -1,28 +1,48 @@
 #[cfg(test)]
 mod tests {
     use mollusk_svm::{result::Check, Mollusk};
-    use solana_sdk::{instruction::Instruction, pubkey::Pubkey};
+    use solana_sdk::{
+        account::AccountSharedData,
+        instruction::{AccountMeta, Instruction},
+        pubkey::Pubkey,
+    };
 
     #[test]
-    fn test_hello_world() {
+    fn withdraw() {
         let program_id_keypair_bytes = std::fs::read("deploy/sbpf-close-keypair.json").unwrap()
             [..32]
             .try_into()
             .expect("slice with incorrect length");
         let program_id = Pubkey::new_from_array(program_id_keypair_bytes);
 
+        let signer = Pubkey::new_unique();
+        let account = Pubkey::new_unique();
+
         let instruction = Instruction::new_with_bytes(
             program_id,
             &[],
-            vec![]
+            vec![
+                AccountMeta::new(signer, true),
+                AccountMeta::new(account, false),
+            ],
         );
 
         let mollusk = Mollusk::new(&program_id, "deploy/sbpf-close");
 
         let result = mollusk.process_and_validate_instruction(
             &instruction,
-            &[],
-            &[Check::success()]
+            &[
+                (signer, AccountSharedData::new(0, 0, &Pubkey::default())),
+                (
+                    account,
+                    AccountSharedData::new(1_000_000_000u64, 32, &program_id),
+                ),
+            ],
+            &[
+                Check::success(),
+                Check::account(&signer).lamports(1_000_000_000).build(),
+                Check::account(&account).lamports(0).build(),
+            ],
         );
         assert!(!result.program_result.is_err());
     }
